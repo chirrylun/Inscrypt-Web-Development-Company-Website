e801129aebe2f023c02d5689830963d0355f7a86
chirrylun <134626109+chirrylun@users.noreply.github.com>
2024-08-29 10:47:45 +0100

    update

---
+++ code.js
@@ -0,0 +1,22 @@
+const sliderContainer = document.querySelector(".slider-container")
+const slides = document.querySelectorAll(".slides")
+const nextBtn = document.getElementById("nextBtn")
+const prevBtn = document.getElementById("prevBtn")
+
+let currentIndex = 0
+
+function updateSlide () {
+
+}
+
+function nextSlide () {
+
+}
+
+function prevSlide () {
+
+}
+
+nextBtn.addEventListener('click', nextSlide)
+prevBtn.addEventListener('click', prevSlide)
+
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      colors:{
+        white: "#FFFFFF",
+        gray: "#686868"
+      },
+      
+      
+    },
+  },
+  plugins: [],
+}
+
